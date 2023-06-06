dcad8477f509d0de1999b89c9c08ec32507fb28e
DiegoloM2 <diego.moreno.manes@hotmail.com>
2023-06-06 17:30:25 +0200

    updated set_angle
    GPIO is no longer initialized, the pwm will now be inputted.
    The function will not be tested in this file, a separate file will be created for this purpose

--- ServoMotors/servo_control.py
+++ ServoMotors/servo_control.py
@@ -1,26 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BCM)
-servo_pin = 27
+# Initialize GPIO settings (not necessary since pwm will be passed as parameter on call)
+# GPIO.setmode(GPIO.BCM)
+# servo_pin = 27
+# GPIO.setup(servo_pin, GPIO.OUT)
+# pwm = GPIO.PWM(servo_pin, 50)
+# pwm.start(2.5)
 
-GPIO.setup(servo_pin, GPIO.OUT)
-pwm = GPIO.PWM(servo_pin, 50)
-pwm.start(2.5)
+def set_angle(angle, pwm):
+    """
+    Set the angle of the servo motor.
 
-def set_angle(angle):
+    Args:
+        angle (float): The desired angle of the servo motor (0-180).
+    """
     duty_cycle = 2.5 + (angle / 18)
     pwm.ChangeDutyCycle(duty_cycle)
     time.sleep(0.3)
 
-try:
-    while True:
-        set_angle(30)
-        print("angle = 30")
-        time.sleep(1)
-        set_angle(90)
-        print("angle = 90")
-        time.sleep(1)
-except KeyboardInterrupt:
-    pwm.stop()
-    GPIO.cleanup()
+
+
+
