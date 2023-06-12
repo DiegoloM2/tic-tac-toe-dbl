c2a8c893892344c7d38f60803ad380efb1eec158
Diego Moreno Mañes <diego.moreno.manes@hotmail.com>
2023-06-12 13:38:54 +0200

    refactored the code for the sorting system

--- code/implementation/sorting.py
+++ code/implementation/sorting.py
@@ -3,41 +3,40 @@ import time
 
 from libraries.Drivers.color_driver.TCS34725_class import TCS34725
 from libraries.Drivers.color_driver.color_functions import *
-
-from libraries.Drivers.LDR_driver.LDR_control import *
-
 from libraries.Drivers.servo_driver.servo_control import *
 
-colourSensor = TCS34725()
-set_angle(90)
 
-def detect_disk(colourSensor):
-    return run_colour_sensor(colourSensor)
+class Sorting():
+    def __init__(self):
+        # Last disk represents the last disk's color (0 if black, 1 if white)
+        self.lastDisk = 0
+        self.colorSensor = TCS34725()
+        set_angle(90)
 
-def detect_light():
-    return read_photoresistor() == 0
+    def detect_disk(self,):
+        return run_colour_sensor(self.colorSensor)
+    def sort_disk(self):
+        set_angle(30)
+        time.sleep(5)
+        set_angle(90)
 
-
-def sort_disk(whiteblack):
-    set_angle(30)
-    time.sleep(5)
-
-def detect_and_sort(colourSensor):
-    whiteblack = 0
-    while True:
-        print(str(detect_light()) + " " + detect_disk(colourSensor))
-        if detect_disk(colourSensor) == "White" and detect_light() and whiteblack == 0:
+    def timeToSort(self, ):
+        if detect_disk(self.colorSensor) == "White" and self.lastDisk == 0:
             print("White Disk Detected -> Time To Sort!")
-
-            sort_disk(whiteblack)
-            whiteblack = 1
-        elif detect_disk(colourSensor) == "White" and detect_light() and whiteblack == 1:
-            print("White Disk Detected -> Not Sorting, Looking For Black!")
-        elif detect_disk(colourSensor) == "Black" and detect_light() and whiteblack == 1:
+            lastDisk = 1
+            return True
+        elif detect_disk(self.colorSensor) == "Black" and self.lastDisk == 1:
             print("Black Disk Detected -> Time To Sort!")
-            sort_disk(whiteblack)
-            whiteblack = 0
-        elif detect_disk(colourSensor) == "Black" and detect_light() and whiteblack == 0:
+            lastDisk = 0
+            return True
+        elif detect_disk(self.colorSensor) == "Black" and self.lastDisk == 0:
             print("Black Disk Detected -> Not Sorting, Looking For White!")
-        else:
-            set_angle(90)
+            return False
+        elif detect_disk(self.colorSensor) == "White" and self.lastDisk == 1:
+            print("White Disk Detected -> Not Sorting, Looking for Black!")
+            return False
+        return False
+
+
+
+
--- code/implementation/test_sorting.py
+++ code/implementation/test_sorting.py
@@ -2,13 +2,14 @@ import pytest
 from sorting import detect_disk, sort_disk, run_colour_sensor, TCS34725
 import time
 
+sorter = Sorting()
+
 def test_detect_white_disk():
     # Set up the necessary objects or sensors for testing
-    colourSensor = TCS34725()
     print("--- TESTING THE DETECT_DISK FUNCTION ---")
     print("PLACE A WHITE DISK IN THE BELT FOR THE SENSOR TO DETECT, YOU HAVE 2 SECONDS")
     time.sleep(2)
-    result = detect_disk(colourSensor)
+    result = sorter.detect_disk()
 
     # Compare the result with the expected outcome
     assert result == "White"
@@ -19,7 +20,7 @@ def test_detect_black_disk():
     print("--- TESTING THE DETECT_DISK FUNCTION ---")
     print("PLACE A BLACK DISK IN THE BELT FOR THE SENSOR TO DETECT, YOU HAVE 2 SECONDS")
     time.sleep(2)
-    result = detect_disk(colourSensor)
+    sorter.detect_disk()
 
     # Compare the result with the expected outcome
     assert result == "Black"
