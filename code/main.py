2a48f861e3823c86e96b426e2db36d3439b8c406
Diego Moreno Manes <diego.moreno.manes@hotmail.com>
2023-06-14 04:18:36 +0200

    refactored main file

--- code/main.py
+++ code/main.py
@@ -3,72 +3,96 @@ from libraries.TicTacToe.tic_tac_toe import *
 from libraries.Grid.grid import *
 from libraries.Drivers.dc_driver.AMSpi import *
 
+# Disable GPIO warnings
 GPIO.setwarnings(False)
 
+# Initialize AMSpi instance
 amspi = AMSpi()
+
+# Set the pins for 74HC595 shift register
 amspi.set_74HC595_pins(21, 20, 16)
+
+# Set the pins for L293D motor driver
 amspi.set_L293D_pins(PWM0B=5, PWM0A=13, PWM2B=19)
+
+# Create Sorting instance for disk sorting
 sorter = Sorting()
+
+# Create an empty Tic Tac Toe board
 board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+# Initialize game variables
 gameChoice = -1
 gameTurn = -1
 player = -1
 
+# Reset the game board to start playing
 reset_board(amspi)
 
-while True:
-    won = analyze_board(board) in [1, -1]
-    if gameTurn > 9 or won:
-        board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-        gameChoice = -1
-        gameTurn = -1
-        player = -1
-        print("--------\nTHIS GAME HAS FINISHED, PLEASE TAKE THE DISKS OF THE GRID AND TYPE any key once your finished so we can continue")
-        input()
-    if sorter.timeToSort():
-        position = -1
-        # If it is time to sort, sort the disk
-        sorter.sort_disk()
 
-        # Check if viewer has decided on how they want to play
-        if gameChoice == -1:
-            gameChoice = chooseGame()
+def reset_game():
+    """
+    Resets the game state by resetting the board and game variables.
+    """
+    global board, gameChoice, gameTurn, player
+    board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    gameChoice = -1
+    gameTurn = -1
+    player = -1
+
+def play_game():
+    """
+    Starts and manages the main loop.
+    """
+    global board, gameChoice, gameTurn, player
+
+    while True:
+        won = analyze_board(board) in [1, -1]
+        if gameTurn > 9 or won:
+            reset_game()
+            print("--------\nTHIS GAME HAS FINISHED, PLEASE TAKE THE DISKS OFF THE GRID AND TYPE any key once you're finished so we can continue")
+            input()
+        
+        if sorter.timeToSort():
+            position = -1
+            
+            # If it's time to sort, sort the disk
+            sorter.sort_disk()
+            
+            # Check if player has chosen the game mode
+            if gameChoice == -1:
+                gameChoice = chooseGame()
+                if gameChoice == 1:
+                    player = input("Enter 1 to play first or 2 to play second: ")
+                    player = int(player)
+                gameTurn = 1
+    
             if gameChoice == 1:
-                player = input("Enter to play 1(st) or 2(nd): ")
-                player = int(player)
-
-            gameTurn = 1
-
-
-
-        if gameChoice == 1:
-
-            if (gameTurn + player) % 2 == 1:
-                print("\n--- COMPUTER'S MOVE ---\n")
-                board, position = CompTurn(board)
-                
-                # Add index for makeMove function since it uses one index and minmax uses zero-index.
-                position += 1
+                if (gameTurn + player) % 2 == 1:
+                    print("\n--- COMPUTER'S MOVE ---\n")
+                    board, position = CompTurn(board)
+                    
+                    # Adjust the position to use one-index for makeMove function
+                    position += 1
+                else:
+                    print("--- PLAYER'S MOVE ---")
+                    board, position = player_turn(board, -1)  # Player 1's turn
             else:
-                
-                print("--- PLAYER'S MOVE ---")
-                board, position = player_turn(board, -1)  # Player 1's turn
-        else:
-            print(f"--- PLAYER {player}'S MOVE ---")
+                print(f"--- PLAYER {player}'S MOVE ---")
+                player = player * -1
+                board, position = player_turn(board, player)
+    
+            makeMove(position, amspi)
+            print("--- FINISHED EXECUTING MOVE ---\n")
+            print("\nCURRENT STATE OF BOARD")
+            display_board(board)
+            print("\n---- CHECKING FOR THE NEXT DISK ----\n\n")
+            gameTurn += 1
+    
+            time.sleep(0.3)
 
-            player = player * -1
-            board, position = player_turn(board, player)
-
-        makeMove(position, amspi)
-        print("--- FINISHED EXECUTING MOVE ---\n")
-        print("\nCURRENT STATE OF BOARD")
-        display_board(board)
-        print("\n---- CHECKING FOR THE NEXT DISK ----\n\n")
-        gameTurn += 1
-
-
-
-        time.sleep(0.3)
+reset_game()
+play_game()
 
 
 
